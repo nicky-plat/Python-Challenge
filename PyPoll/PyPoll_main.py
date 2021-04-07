558e70cf59ae12183e1dad116c92bfcad8c1b894
nicky-plat <nhanc05.79@gmail.com>
2021-04-07 12:47:29 -0400

    Updates to PyPoll_main.py file

--- PyPoll/PyPoll_main.py
+++ PyPoll/PyPoll_main.py
@@ -1,1 +1,31 @@
-# ToDo pseudo code before I begin
+# import modules / dependencies
+import os
+import csv
+
+csv_file_path = os.path.join(".", "Resources", "election_data.csv")
+# print(csv_file_path)
+# Initialize variables
+total_votes = 0
+total_khan = 0
+total_correy = 0
+total_li = 0
+total_otooley = 0
+winner = 0
+
+
+
+
+output = (
+    f"Election Results\n"
+    f"------------------------------------"
+    f"Total Votes: {total_votes}\n"
+    f"------------------------------------"
+    f"Khan: {total_khan}\n"
+    f"Correy: {total_correy}\n"
+    f"Li: {total_li}\n"
+    f"O'Tooley: {total_otooley}\n"
+    f"------------------------------------"
+    f"Winner: {winner}\n"
+)
+
+print(output)
