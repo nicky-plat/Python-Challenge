58fd8b2e851dbd9b9bd8ce389b5b17a6e8892b1a
nicky-plat <nhanc05.79@gmail.com>
2021-04-09 13:39:22 -0400

    Updates to assigment

--- PyPoll/PyPoll_main.py
+++ PyPoll/PyPoll_main.py
@@ -1,4 +1,5 @@
 # import modules / dependencies
+from collections import Counter
 import os
 import csv
 
@@ -10,34 +11,50 @@ total_khan = 0
 total_correy = 0
 total_li = 0
 total_otooley = 0
+name_khan = 0
+name_correy = 0
+name_li = 0
+name_otooley = 0
 winner = 0
-candidates = {}
+counts = {}
 
-with open(csv_file_path, newline='') as csv_file:
-    csv_reader = csv.reader(csv_file, delimiter= ",")
-    
+with open(csv_file_path, 'r') as csv_file:
+    csv_reader = csv.DictReader(csv_file)
+
+    # creating empty lists
+    voterID = []
+    county = []
+    candidate = []
+    total_votes = 0
+    total_khan = 0
+    total_correy = 0
+    total_li = 0
+    total_otooley = 0
+    name_khan = []
+    name_correy = []
+    name_li = []
+    name_otooley = []
+    winner = 0
+    counts = {}
     # # Read the Header
-    csv_header = next(csv_file)
-    print(f"Header: {csv_header}")
-
+    # csv_header = next(csv_file)
+    # print(f"Header: {csv_header}")
+    count = 0
     counter = 0
-    candidates = []
     previous_candidate = 0
-    for row in csv_reader:
-        # calculate total votes
-        total_votes = total_votes + 1
-        candidates = str(row[2])
-        candidates = set(candidates)
-        print(candidates)
 
-        # # create variable to hold candidates
-        # for name in candidates:
-        #     # if counter >= 1
-        #     #     candidates.append(str(row[2])
-        #     # if row[2] == previous_candidate
-        #     # print(candidates)
+    # create variable to hold candidates
+    for col in csv_reader:
+        voterID.append(col['Voter ID'])
+        county.append(col['County'])
+        candidate.append(col['Candidate'])
+        # print('Candidates:', candidate)
+        for name in candidate:
+            if name == "Khan":
+                name_khan.append(name)
+                total_khan = name_khan.count
+                print(total_khan)
             
-
 # output = (
 #     f"Election Results\n"
 #     f"------------------------------------\n"
@@ -50,5 +67,4 @@ with open(csv_file_path, newline='') as csv_file:
 #     f"------------------------------------\n"
 #     f"Winner: {winner}\n"
 # )
-
 # print(output)
